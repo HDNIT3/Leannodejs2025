5087f707627f762dc43801e2b8681946f159b986
HDNIT3 <nguyenhuynh.663459@gmail.com>
2025-01-19 15:39:38 +0700

    s

--- index.js
+++ index.js
@@ -1,28 +1,44 @@
-const express = require('express');
+const express = require("express");
 const bodyParser = require("body-parser");
 const app = express();
 const port = 3000;
 
+const hello = [
+  {
+    id: 1,
+    name: "nguyen",
+  },
+  {
+    id: 2,
+    name: "nguyen2",
+  },
+];
 
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended : true}))
+app.set("view engine", "ejs");
+app.set("views", "./views");
 
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get('/:id', (req , res) => {
-    const { id:id } = req.params
-    res.json(`This is ${id}`)
-})
+app.get("/", (req, res) => {
+  res.render("index");
+});
 
-app.get("/d/IO", (req , res) => {
-    const a = req.query
-    res.json(a)
-})
+app.get("/:id", (req, res) => {
+  const { id: id } = req.params;
+  res.json(`This is ${id}`);
+});
 
-app.post("/create",(req,res)=>{
-    res.json(req.body)
-    res.json("Create")
-})
+app.get("/d/IO", (req, res) => {
+  const a = req.query;
+  res.json(a);
+});
+
+app.post("/create", (req, res) => {
+  res.json(req.body);
+  res.json("Create");
+});
 
 app.listen(port, () => {
-    console.log("chao")
-})
+  console.log("chao");
+});
