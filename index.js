f7a5d85710fc9181319dfc9f0977ffd08b1b31fa
HDNIT3 <nguyenhuynh.663459@gmail.com>
2025-01-19 15:19:16 +0700

    d

--- index.js
+++ index.js
@@ -1,15 +1,28 @@
 const express = require('express');
+const bodyParser = require("body-parser");
 const app = express();
-
-
-const hostname = 'localhost';
 const port = 3000;
 
 
-app.get("/",(req,res)=>{
-    res.send('<h1>Hello word chào </h1>')
+app.use(bodyParser.json())
+app.use(bodyParser.urlencoded({extended : true}))
+
+
+app.get('/:id', (req , res) => {
+    const { id:id } = req.params
+    res.json(`This is ${id}`)
 })
 
-app.listen(port,hostname, () => {
-    console.log(`http://${hostname}:${port}/`)
+app.get("/d/IO", (req , res) => {
+    const a = req.query
+    res.json(a)
+})
+
+app.post("/create",(req,res)=>{
+    res.json(req.body)
+    res.json("Create")
+})
+
+app.listen(port, () => {
+    console.log("chao")
 })
